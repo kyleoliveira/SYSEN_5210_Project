d661d82966a8adee8a755cf96933dc495006aaf5
Kyle Oliveira <kcoliveira@ucdavis.edu>
2016-04-17 21:51:18 -0400

    Refactored to use AASM to represent the Aircraft state machine

--- aircraft.rb
+++ aircraft.rb
@@ -1,4 +1,5 @@
 require 'distribution'
+require 'aasm'
 
 # An aircraft for use in the simulation
 class Aircraft
@@ -9,8 +10,73 @@ class Aircraft
                 :queuing_time,  # Time remaining until the aircraft reaches the threshold point
                 :circling_time, # Time remaining until the aircraft has circled around
                 :landing_time,  # Time remaining until the aircraft has cleared the landing zone
-                :status         # The aircraft's current state, one of:
-                                #   :approaching, :queuing, :circling, :landing
+                :flight_number  # Silly identifier
+
+  include AASM
+
+  # Define a state machine to represent how the aircraft state changes throughout the simulation
+  aasm do
+
+    # Aircraft states
+    state :contacted, initial: true
+    state :approaching
+    state :queuing
+    state :circling
+    state :landing
+    state :done
+
+    # Events that transition an aircraft between states
+    event :approach do
+      transitions from: [:contacted, :approaching], to: :approaching, unless: :is_at_queue?
+      after do
+        @approaching_time -= 1 unless @approaching_time < 1
+      end
+    end
+
+    event :start_queuing, after: :queue_up do
+      transitions from: [:approaching, :circling], to: :queuing, if: :is_at_queue?
+    end
+
+    event :queue do
+      transitions from: :queuing, to: :queuing, unless: :is_at_threshold_point?
+      after do
+        @queuing_time -= 1 unless @queuing_time < 1
+      end
+    end
+
+    event :start_circling do
+      transitions from: :queuing, to: :circling, if: :is_at_threshold_point?
+      after do
+        @circling_time = positive_normal_random_number(750, 150)
+      end
+    end
+
+    event :circle do
+      transitions from: :circling, to: :circling, unless: :is_done_circling?
+      after do
+        @circling_time -= 1 unless @circling_time < 1
+      end
+    end
+
+    event :start_landing do
+      transitions from: :queuing, to: :landing, if: :is_at_threshold_point?
+      after do
+        @landing_time = positive_normal_random_number(750, 150)
+      end
+    end
+
+    event :land do
+      transitions from: :landing, to: :landing, unless: :is_done_landing?
+      after do
+        @landing_time -= 1 unless @landing_time < 1
+      end
+    end
+
+    event :finish do
+      transitions from: :landing, to: :done, if: :is_done_landing?
+    end
+
+  end
 
   # Initializes the Aircraft's state
   def initialize
@@ -25,129 +91,95 @@ class Aircraft
       @type = :small
     end
 
-    generate_arrival_time
-    start_approaching
+    @flight_number = random_flight_number
+
+    @arrival_time = 0
+    @approaching_time = 0
     @queuing_time = 40
     @circling_time = 0
     @landing_time = 0
 
+    generate_arrival_time
+    generate_approaching_time
+  end
+
+  def positive_normal_random_number(mu, sigma)
+    result = Distribution::Normal.rng(mu, sigma).call.ceil.to_i
+    until result >= 0
+      result = Distribution::Normal.rng(mu, sigma).call.ceil.to_i
+    end
+    result
   end
 
   # Generates a random arrival time based on N(180, 60)
   def generate_arrival_time
-    @arrival_time = Distribution::Normal.rng(180, 60).call
-    until @arrival_time >= 0
-      @arrival_time = Distribution::Normal.rng(180, 60).call
+    @arrival_time = positive_normal_random_number(180, 60)
+  end
+
+  # Generates a random approach time based on N(180, 60)
+  def generate_approaching_time
+    @approaching_time = positive_normal_random_number(600, 150)
+  end
+
+  def airlines
+    ['SATA', 'TAP', 'United', 'American', 'Delta']
+  end
+
+  def random_flight_number
+    "#{airlines.sample} #{(800..4000).to_a.sample}"
+  end
+
+  #
+  # Methods that will be triggered on state machine transitions
+  #
+
+  # Starts the aircraft queuing.
+  # If the aircraft arriving at the landing queue finds the queue empty,
+  # then it takes about 40 seconds to proceed to the threshold point.
+  # Otherwise, the aircraft may potentially spend more time in the landing queue.
+  # This additional time is based on the type of the aircraft and the aircraft preceding it and is
+  # calculated based on the mean and standard deviation times provided.
+  def queue_up(lead)
+    if lead.nil?
+      @queuing_time = 40
+    else
+      @queuing_time = separation_from(lead)
     end
   end
 
-  #
-  # Methods to handle circling in the landing queue:
-  #
-
-  # Advance the amount of time the aircraft has been circling, if necessary
-  def circle
-    start_queuing if @circling_time == 1
-    @circling_time -= 1 unless @circling_time < 1
+  # Given that the aircraft is approaching,
+  # determines if it has reached the landing queue.
+  # @return [TrueFalse] true if the aircraft is at the queue
+  def is_at_queue?
+    @approaching_time == 0
   end
 
-  # Starts the aircraft circling
-  def start_circling
-    @circling_time = Distribution::Normal.rng(750, 150).call
-    until @circling_time >= 0
-      @circling_time = Distribution::Normal.rng(750, 150).call
-    end
-
-    @status = :circling
+  # Given that the aircraft is circling,
+  # determines if it has reached the landing queue.
+  # @return [TrueFalse] true if the aircraft is at the queue
+  def is_done_circling?
+    @circling_time == 0
   end
 
-  def is_circling?
-    @status == :circling
-  end
-
-  #
-  # Methods to handle approaching the landing queue:
-  #
-
-  # Advance the amount of time the aircraft has been approaching, if necessary
-  def approach
-    start_queuing if @approaching_time == 1
-    @approaching_time -= 1 unless @approaching_time < 1
-  end
-
-  # Starts the aircraft circling
-  def start_approaching
-    @approaching_time = Distribution::Normal.rng(600, 150).call
-    until @approaching_time >= 0
-      @approaching_time = Distribution::Normal.rng(600, 150).call
-    end
-
-    @status = :approaching
-  end
-
-  def is_approaching?
-    @status == :approaching
-  end
-
-  #
-  # Methods for handling landing
-  #
-
-  # Advance the amount of time the aircraft has been approaching, if necessary
-  def queue
-    @queuing_time -= 1 unless @queuing_time < 1
-  end
-
-  # Starts the aircraft queuing
-  def start_queuing
-    @queuing_time = 40
-    @status = :queuing
-  end
-
-  def is_queuing?
-    @status == :queuing
-  end
-
+  # Given that the aircraft is queuing,
+  # determines if it has reached the threshold point.
+  # @return [TrueFalse] true if the aircraft is at the threshold point
   def is_at_threshold_point?
-    @status == :queuing && @queuing_time == 0
+    @queuing_time == 0
   end
 
-  # Advance the amount of time the aircraft has been landing, if necessary
-  def land
-    @landing_time -= 1 unless @landing_time < 1
+  # Given that the aircraft is landing,
+  # determines if it is done landing.
+  # @return [TrueFalse] true if the aircraft is done landing
+  def is_done_landing?
+    @landing_time == 0
   end
-
-  # Starts the aircraft landing
-  def start_landing
-    @landing_time = Distribution::Normal.rng(750, 150).call
-    until @landing_time >= 0
-      @landing_time = Distribution::Normal.rng(750, 150).call
-    end
-
-    @status = :landing
-  end
-
-  def is_landing?
-    @status == :landing
-  end
-
-  #
-  # Methods for handling separation
-  #
 
   # Returns an appropriately generated separation time
   # given the aircraft in front of this aircraft.
   def separation_from(lead)
-    separation = Distribution::Normal.rng(Aircraft::separation_mean[lead.type][self.type],
-                                          Aircraft::separation_sd[lead.type][self.type])
-                                     .call
-    until separation >= 0
-      separation = Distribution::Normal.rng(Aircraft::separation_mean[lead.type][self.type],
-                                            Aircraft::separation_sd[lead.type][self.type])
-                       .call
-    end
-
-    separation
+    positive_normal_random_number(Aircraft::separation_mean[lead.type][self.type],
+                                  Aircraft::separation_sd[lead.type][self.type])
   end
 
 
--- simulation.rb
+++ simulation.rb
@@ -4,69 +4,74 @@ require './aircraft.rb'
 class Simulation
 
   attr_accessor :future_arrivals, # A list of generated arrivals
-                :landing_queue,   # The queue of aircraft that are ready to land. Some may be circling
-                :landing_zone,    # The aircraft on the runway that are approaching the simulation exit
+                :approaching_queue, # The queue of aircraft that have made initial contact and approaching the landing queue
+  #               :landing_queue,   # The queue of aircraft that are ready to land. Some may be circling
+  #               :landing_zone,    # The aircraft on the runway that are approaching the simulation exit
                 :sim_time         # The current simulation time in seconds
 
   # Sets up the simulation
   # @param [Integer] arrival_count The number of arrivals to generate for the simulation
-  def initialize(arrival_count=10)
+  def initialize(arrival_count=10, filename='simulation.out')
+    @output_file = open(filename, 'w')
+
     @sim_time = 0
-    @circling_queue = []
-    @landing_queue = []
-    @landing_zone = []
+    @approaching_queue = []
+    # @circling_queue = []
+    # @landing_queue = []
+    # @landing_zone = []
     @future_arrivals = []
 
     # Generate a given number of arrivals to process, and sort the list based on arrival time
     arrival_count.times do
       @future_arrivals << Aircraft.new
     end
-    @future_arrivals.sort{ |left, right| left.arrival_time <=> right.arrival_time }
+    @future_arrivals.sort!{ |left, right| left.arrival_time.to_i <=> right.arrival_time.to_i }
   end
 
   # Adds any new arrivals at the current time to the landing queue
   def process_arrivals
     # Move any new arrivals from the future arrivals list to end of the landing queue
-    while @future_arrivals.first.arrival_time == @sim_time
-      @future_arrivals.first.start_queuing
-      @landing_queue << @future_arrivals.slice!(0)
+    while future_arrivals.length > 0 && future_arrivals.first.arrival_time == sim_time
+      future_arrivals.first.approach!
+      approaching_queue << future_arrivals.slice!(0)
+      @output_file.write "\n#{approaching_queue.last.flight_number} made contact at T=#{sim_time}\n"
     end
   end
-
-  # Updates the landing queue state
-  def process_circling
-    # Decrement the circling time for everyone who is already in the queue
-    @circling_queue.each { |aircraft| aircraft.circle }
-
-    # Move any aircraft that are done circling to the landing queue
-    @circling_queue.each do |aircraft|
-      if aircraft.is_queuing?
-        @landing_queue << aircraft
-      end
-    end
-    @circling_queue.delete_if { |aircraft| aircraft.is_queuing? }
-  end
-
-  # Updates the landing queue state
-  def process_landing_queue
-    # Increment queuing time for all the aircraft that are queuing
-    @landing_queue.each { |aircraft| aircraft.queue }
-  end
-
-  # Updates the landing zone state
-  def process_landing_zone
-    # Continue processing anyone who is still in the landing zone
-    @landing_zone.each { |aircraft| aircraft.land }
-
-    # If the zone is clear, allow a new aircraft to enter
-    if @landing_zone
-
-    # For any aircraft remaining in the landing queue but:
-    # * have not started circling
-    # * have reached the threshold point
-    # have them start circling
-
-  end
+  #
+  # # Updates the landing queue state
+  # def process_circling
+  #   # Decrement the circling time for everyone who is already in the queue
+  #   @circling_queue.each { |aircraft| aircraft.circle }
+  #
+  #   # Move any aircraft that are done circling to the landing queue
+  #   @circling_queue.each do |aircraft|
+  #     if aircraft.is_queuing?
+  #       @landing_queue << aircraft
+  #     end
+  #   end
+  #   @circling_queue.delete_if { |aircraft| aircraft.is_queuing? }
+  # end
+  #
+  # # Updates the landing queue state
+  # def process_landing_queue
+  #   # Increment queuing time for all the aircraft that are queuing
+  #   @landing_queue.each { |aircraft| aircraft.queue }
+  # end
+  #
+  # # Updates the landing zone state
+  # def process_landing_zone
+  #   # Continue processing anyone who is still in the landing zone
+  #   @landing_zone.each { |aircraft| aircraft.land }
+  #
+  #   # If the zone is clear, allow a new aircraft to enter
+  #   if @landing_zone
+  #
+  #   # For any aircraft remaining in the landing queue but:
+  #   # * have not started circling
+  #   # * have reached the threshold point
+  #   # have them start circling
+  #
+  # end
 
   # Runs the simulation
   # @param [Integer] duration The amount of time to run the simulation for, in seconds
@@ -75,15 +80,19 @@ class Simulation
 
       process_arrivals
 
-      process_circling
-
-      process_landing_zone
-
-      process_landing_queue
+      # process_circling
+      #
+      # process_landing_zone
+      #
+      # process_landing_queue
 
       # Advance the simulation time
+      @output_file.write '.'
       @sim_time += 1
     end
+
+    # Close the file since we're done writing it.
+    @output_file.close
   end
 
 end
