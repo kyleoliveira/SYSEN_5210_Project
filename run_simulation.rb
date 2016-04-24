6b4636e314a60574e9939755ffa02c22841ef819
Kyle Oliveira <kyle.oliveira@cornell.edu>
2016-04-24 17:41:11 -0400

    Refactors the simulation runner so that it is easier to work with.

--- run_simulation.rb
+++ run_simulation.rb
@@ -3,9 +3,10 @@
 require './simulation.rb'
 
 reps = (ARGV[0] || 10).to_i
-filename = ARGV[1] || 'simulation'
+aircraft = (ARGV[1] || 10).to_i
+filename = ARGV[2] || 'simulation'
 
 reps.times do |i|
-  sim = Simulation.new(10, "./results/#{filename}_#{i.to_s.rjust(2, '0')}.csv")
+  sim = Simulation.new(aircraft, "./results/#{filename}_#{i.to_s.rjust(2, '0')}.csv")
   sim.run!
 end
